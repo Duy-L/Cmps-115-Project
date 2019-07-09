cf9436cceb50f0e91b70043bbb049a44529f0738
Duy Le <duy@eduroam-169-233-141-82.ucsc.edu>
2019-07-08 17:28:51 -0700

    Cart Functionality

--- src/products/models.py
+++ src/products/models.py
@@ -13,8 +13,6 @@ def get_filename_ext(filepath):
 	return name, ext
 
 def upload_image_path(instance, filename):
-	#print(instance)
-	#print(filename)
 	new_filename = random.randint(1,234423425)
 	name, ext = get_filename_ext(filename)
 	final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
