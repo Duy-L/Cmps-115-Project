156e2bd1c2ac75c33849d097535fe27b394fbc2f
TonyTVKay <tonytvkay@gmail.com>
2019-07-10 23:58:44 -0700

    urls.py update for adding products

--- src/products/urls.py
+++ src/products/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import url
 from .views import (
     ProductListView, 
     ProductDetailSlugView, 
+    ProductCreateView
     )
 
 
 urlpatterns = [
     url(r'^$', ProductListView.as_view(), name='list'),
+    url(r'new', ProductCreateView.as_view(), name='product-create'),
     url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail'),
 ]
 
