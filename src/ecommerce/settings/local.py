768b57b8f4645088dfb9a24c8c4815cc1a1873de
Duy Le <banle@ucsc.edu>
2019-07-18 01:58:42 -0700

    updated for heroku

--- src/ecommerce/settings/local.py
+++ src/ecommerce/settings/local.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -160,3 +160,15 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 #Django Paypal settings
 PAYPAL_RECEIVER_EMAIL = 'azeynalo@ucsc.edu'
 PAYPAL_TEST = True
+
+
+# from https://www.codingforentrepreneurs.com/blog/ssltls-settings-for-django/
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
