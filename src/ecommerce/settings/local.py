862b4faff81c80a6bb602d2e7da4896bc75894c0
xwu63 <52588519+xwu63@users.noreply.github.com>
2019-07-18 13:29:34 -0700

    Update local.py

--- src/ecommerce/settings/local.py
+++ src/ecommerce/settings/local.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
+    
+    #third party
+    'storages'
+    
     #our apps
     'allauth',
     'allauth.account',
