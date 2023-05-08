5da62df597567d1856e4bc0138e462aefd6d7ff1
xwu63 <52588519+xwu63@users.noreply.github.com>
2023-05-08 13:49:55 -0700

    Update utils.py

--- src/ecommerce/aws/utils.py
+++ src/ecommerce/aws/utils.py
@@ -1,5 +1,1 @@
-from storages.backends.s3boto3 import S3Boto3Storage
 
-#this takes care of our static pictures and media pictures and upload to the AWS S3 cloud
-StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
-MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
