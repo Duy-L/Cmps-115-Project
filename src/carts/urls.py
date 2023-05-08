17a7110daa268c75200ebcf1df9c296150f718e1
xwu63 <52588519+xwu63@users.noreply.github.com>
2023-05-08 13:46:42 -0700

    Update urls.py

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -1,16 +1,1 @@
-from django.conf.urls import url
 
-from .views import (
-    cart_home,
-    cart_update_remove,
-    cart_update_add,
-    cart_clear,
-    )
-
-
-urlpatterns = [
-    url(r'^$', cart_home, name='home'),
-    url(r'^update_remove/$', cart_update_remove, name='update_remove'),
-    url(r'^update_add/$', cart_update_add, name='update_add'),
-    url(r'^cart_clear/$', cart_clear, name ='cart_clear')
-]
