2aa00a80ed36a54773453c4d6c7c40ac2ba60420
xwu63 <52588519+xwu63@users.noreply.github.com>
2023-05-08 18:36:57 -0700

    Update admin.py

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,11 +1,2 @@
-from django.contrib import admin
-from .models import Product
 
-#access to the products in the /admin page
-class ProductAdmin(admin.ModelAdmin):
-	list_display = ['__str__', 'slug']
-	class Meta:
-		model = Product
-
-admin.site.register(Product, ProductAdmin)
 
