082b45ed88a92879d297e73182d7e60dfd4742e1
xwu63 <52588519+xwu63@users.noreply.github.com>
2023-05-08 18:16:16 -0700

    Update 0003_product_image.py

--- src/products/migrations/0003_product_image.py
+++ src/products/migrations/0003_product_image.py
@@ -5,14 +5,4 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('products', '0002_product_price'),
-    ]
-
-    operations = [
-        migrations.AddField(
-            model_name='product',
-            name='image',
-            field=models.FileField(blank=True, null=True, upload_to='products/'),
-        ),
     ]
